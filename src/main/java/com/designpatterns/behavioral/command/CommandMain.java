874db8eab200c5e08f71d355c6bdebc6749b3595
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-07-21 12:18:59 +0000

    Refactor command design pattern.

--- src/main/java/com/designpatterns/behavioral/command/CommandMain.java
+++ src/main/java/com/designpatterns/behavioral/command/CommandMain.java
@@ -4,7 +4,7 @@ public class CommandMain {
 
     public static void main(String[] args) {
         TextFileOperationExecutor executor = new TextFileOperationExecutor();
-        executor.executeOperaion(() -> "SaveFile.txt");
-        executor.executeOperaion(() -> "OpenFile.txt");
+        executor.executeOperaion(new SaveTextFile(new TextFile("SaveFile.txt")));
+        executor.executeOperaion(new OpenTextFile(new TextFile("OpenFile.txt")));
     }
 }
